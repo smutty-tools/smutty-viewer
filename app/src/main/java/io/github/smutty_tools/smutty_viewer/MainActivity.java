00302da84cc15ac150f3acef1916a34baab3d433
smutty-tools <smutty-tools@users.noreply.github.com>
2017-11-18 17:04:50 +0100

    add options menu to main activity

--- app/src/main/java/io/github/smutty_tools/smutty_viewer/MainActivity.java
+++ app/src/main/java/io/github/smutty_tools/smutty_viewer/MainActivity.java
@@ -2,6 +2,9 @@ package io.github.smutty_tools.smutty_viewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true; // display the menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_share:
+                return true; // consume the event
+            case R.id.action_refresh:
+                return true; // consume the event
+            case R.id.action_settings:
+                return true; // consume the event
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
