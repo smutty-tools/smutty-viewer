1bf9f879248e60ee99b2bc7548bb3e7f6e091097
smutty-tools <smutty-tools@users.noreply.github.com>
2017-11-18 17:44:17 +0100

    add SettingsActivity, reachable from OptionsMenu

--- app/src/main/java/io/github/smutty_tools/smutty_viewer/MainActivity.java
+++ app/src/main/java/io/github/smutty_tools/smutty_viewer/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.smutty_tools.smutty_viewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_refresh:
                 return true; // consume the event
             case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
                 return true; // consume the event
             default:
                 return super.onOptionsItemSelected(item);
