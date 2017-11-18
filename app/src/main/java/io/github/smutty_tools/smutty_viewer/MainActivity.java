6dbde1988ce294f06eb86c250a2b2b8a56c7e084
smutty-tools <smutty-tools@users.noreply.github.com>
2017-11-18 16:46:33 +0100

    create app using android studio 3.0

---
+++ app/src/main/java/io/github/smutty_tools/smutty_viewer/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.smutty_tools.smutty_viewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
