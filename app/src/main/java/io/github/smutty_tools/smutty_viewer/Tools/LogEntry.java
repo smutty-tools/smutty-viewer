e03ec8b0c86bd960e83e72588d3179349d1475c0
smutty-tools <smutty-tools@users.noreply.github.com>
2017-12-23 13:59:29 +0100

    simpler access by making it read-only

--- app/src/main/java/io/github/smutty_tools/smutty_viewer/Tools/LogEntry.java
+++ app/src/main/java/io/github/smutty_tools/smutty_viewer/Tools/LogEntry.java
@@ -2,19 +2,11 @@ package io.github.smutty_tools.smutty_viewer.Tools;
 
 public class LogEntry {
 
-    private int level;
-    private String message;
+    public final int level;
+    public final String message;
 
     public LogEntry(int level, String message) {
         this.level = level;
         this.message = message;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
